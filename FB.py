a44972876fd8fcb2ed0d91c466f8426b632b186c
edenwzg <edenwzg@outlook.com>
2017-11-27 21:56:15 +0800

    home

--- FB.py
+++ FB.py
@@ -1,8 +1,34 @@
-def reverse(sequence):
-    ''' Return the Reverse sequence. '''
-    return sequence[::-1]
+FORBIDDEN = ('!', '?', '.', ',', ' ')
+
+def reverse(string):
+    '''
+    Reverse the character in the string. 
+    
+    :param1: this is a string to be processed
+    :return: a copy of the string S
+    '''
+    return string[::-1]
 
 
-def is_palindrome(sequence):
-    ''' Return the sequence is palindrome ? '''
-    return sequence == reverse(sequence)
+def is_palindrome(string):
+    ''' 
+    Check if the string is palindrome.
+
+    :param1: this is a string to be processed
+    :return: boolean 
+    '''
+    return string == reverse(string)
+
+
+def remove_forbidden(string, forbidden=','):
+    '''
+    Remove the forbidden char.
+
+    :param1: this is a string to be processed
+    :param2: this is the deleted character sequence
+    :return: a copy of the string S
+    '''
+    for char in forbidden:
+        if char in string:
+            string = string.replace(char, '')
+    return  string
