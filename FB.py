adc92891d0dbf654547052ca8348a6c3d32b86c4
edenwzg <edenwzg@outlook.com>
2017-12-03 21:37:49 +0800

    none

--- FB.py
+++ FB.py
@@ -1,4 +1,7 @@
+#coding = utf-8
+
 FORBIDDEN = ('!', '?', '.', ',', ' ')
+SEPARATOR = ('_', '-','.')
 
 def reverse(string):
     '''
@@ -47,7 +50,7 @@ def make_change(amount, coins=[1, 5, 10, 25], hand=None):
         for result in make_change(amount - coin, coins = coins, hand = hand + [coin]):
             yield result
 
-def Fibonacci(max):
+def fibonacci(max):
     '''
     Fibonacci number.
 
@@ -61,22 +64,147 @@ def Fibonacci(max):
     raise StopIteration('Done')
 
 
-class Counter(object):
-    def __init__(self, low, high):
-        self.current = low
-        self.high = high
-
-    def __iter__(self):
-        return self
-
-    def next(self):
-        if self.current < self.high:
-            self.current += 1
-            return self.current -1
-        raise StopIteration
-
-
-def Counter_generator(low, high):
+def counter(low, high):
+    '''
+    Counter for genterator.
+    '''
     while low <= high:
         yield low
         low += 1
+
+
+def get_part(filename, initchar='', symbols='-_.'):
+    '''
+    Find the keyword in a filename. 
+    
+    :param1: this is a string to be processed
+    :param2: search for initials
+    :param3: split symobls for str
+    :return: a string with '_' intervals 
+
+    :example: 
+        in [1]: filename = '12_dioguitar23.net_MKBD-S109-s30d_H265-1080P-264_482.mp4'
+        in [2]: get_part(filename, 's')
+        Out[2]: 'S109_S30D'
+    '''
+
+    import re
+    import os
+
+    main_name = splitf(filename)
+    symbols = '[' + symbols + ']+'    
+    parts = re.split(symbols, main_name.strip().replace(' ', '_'))
+    result = []
+
+    if initchar.upper() not in main_name.upper():
+        # Not fund
+        return main_name
+    elif initchar == '':
+        # Find number
+        print 'a'
+        [result.append(part) for part in parts if part.isdigit()] 
+    else:
+        # Find specified initial char
+        [result.append(part.upper()) for part in parts if part[0].upper() == initchar.upper()]
+    
+    return '_'.join(result)
+
+
+def splitf(filename, part="main"):
+    '''
+    Rerurn the extension from a filename.
+    :param1: this is a string to be processed
+    :param2: 
+        part='main' return the main name
+        part='ext' return the extension name
+    :return: string
+    '''
+
+    import os
+
+    if part == 'main':
+        result = os.path.splitext(filename)[0]
+    if part == 'ext':
+        result = os.path.splitext(filename)[1]
+
+    return result
+
+
+def print_dict(d, flip=False): 
+    '''
+    '''
+    if flip:
+        for k, v in d.items():
+            print '%-20s%s' % (v,k)
+    else:
+        for k, v in d.items():
+            print '%-50s%s' % (k,v)
+
+
+def write_dict(h, d, flip=True): 
+    '''
+    '''
+    if flip:
+        for k, v in d.items():
+            h.write('%-20s%s\n' % (v,k))
+    else:
+        for k, v in d.items():
+            h.write('%-50s%s\n' % (k,v))          
+
+
+
+def modify_filename_from_file(file, dir):
+    '''
+    Read the file to modify the filename.
+    
+    MKBD_S61.jpg        dioguitar23.net_MKBD-S61.jpg
+    front part          back part
+
+    front part is the current file name
+    back part is the name to be modified
+
+    :param1: file path for Record file name
+    :param2: the directory for modify the file name
+    :return: None
+    
+    '''
+    import os
+    from os.path import join
+
+    for line in open(file, 'r'):
+        os.rename(join(dir, line.split()[0]), join(dir, line.split()[1]))
+
+
+def modify_filename(file, dir, base, char):
+    '''
+    Modify file name.
+
+    :param1: 
+    :param2: 
+    :param3: 
+    :param4: 
+    '''
+
+    import os
+    from os.path import join
+    from os.path import isfile
+
+    f = open(file, 'w')
+    newnames = {}
+    try:
+        for name in os.listdir(dir):
+            if isfile(join(dir, name)) == True:
+                newnames[name] = '%s_%s%s' % (base, get_part(name, char), splitf(name, part='ext'))
+                os.rename(join(dir,name), join(dir, newnames[name]))
+        
+        print_dict(newnames, flip=True)
+        write_dict(f, newnames)
+        print "%s files done!" % len(newnames)
+    finally:
+        f.close()
+
+
+# ----------------------------------------------------------------------
+
+if __name__ == '__main__':
+    pass
